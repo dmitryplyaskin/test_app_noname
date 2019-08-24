ade5094233c6141b08d2dc05f726b2bdc342981d
Dmitry <dima28rus97@gmail.com>
2019-08-24 23:49:01 +1000

    up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { styled } from 'linaria/react';
 
-export const AppHeader = styled.header`
-  text-align: center;
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-`;
 
-export const AppLink = styled.a`
-  color: #61dafb;
-`;
-
-export const AppLogo = styled.img`
-  animation: App-logo-spin infinite 20s linear;
-  height: 40vmin;
-  pointer-events: none;
-
-  @keyframes App-logo-spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-`;
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <AppHeader>
-          <AppLogo src={logo} alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <AppLink
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </AppLink>
-        </AppHeader>
-      </div>
-    );
-  }
+const App = () => {
+  return null
 }
-
 export default App;
